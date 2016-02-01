50cc580ea1a618eb9e1fdf5b3565d069073fe65a
Thinh Le <tle@kemper.com>
2016-02-01 14:43:32 -0500

    added test files

---
+++ files/recipes/create_files.rb
@@ -0,0 +1,7 @@
+file "Create a file" do
+  content "<html><head></head><body><p>AWS Web Server TEST page - Opsworks</p><p>SBX-DistsysTestStack1-698206202.us-west-2.elb.amazonaws.com</p></body></html>"
+  group "root"
+  mode "0755"
+  owner "ec2-user"
+  path "/var/www/index.html"
+end
