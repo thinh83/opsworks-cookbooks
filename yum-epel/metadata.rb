cd77ae924e6f0ba0b0a0bdf2a5e4d5aacb6d8014
Thinh Le <tle@kemper.com>
2016-02-03 10:11:33 -0500

    added yum-epel cookbook

---
+++ yum-epel/metadata.rb
@@ -0,0 +1,15 @@
+name 'yum-epel'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Installs and configures the EPEL Yum repository'
+version '0.6.5'
+
+depends 'yum', '~> 3.2'
+
+%w(amazon centos fedora oracle redhat scientific).each do |os|
+  supports os
+end
+
+source_url 'https://github.com/chef-cookbooks/yum-epel' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/yum-epel/issues' if respond_to?(:issues_url)
