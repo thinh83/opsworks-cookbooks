9caf02ad3bddfaabd4799ac73ee7908c02be9410
Thinh Le <tle@kemper.com>
2016-02-04 14:54:17 -0500

    configured mod proxy

--- files/recipes/default.rb
+++ files/recipes/default.rb
@@ -1,3 +1,11 @@
+execute 'a2enmod proxy' do
+  command 'sudo a2enmod proxy'
+end
+
+execute 'a2enmod proxy' do
+  command 'sudo a2enmod proxy_http'
+end
+
 file "Create a file" do
   content "<html><head></head><body><p>AWS Web Server TEST page - Opsworks</p><p>SBX-DistsysTestStack1-698206202.us-west-2.elb.amazonaws.com</p></body></html>"
   group "root"
