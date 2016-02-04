4e90ea74d98325ac6f295e40cb12cfc74d1b80b6
Thinh Le <tle@kemper.com>
2016-02-04 16:18:23 -0500

    fixed proxypass unterminated error

--- files/recipes/proxypass.rb
+++ files/recipes/proxypass.rb
@@ -2,6 +2,6 @@ cookbook_file "Copy a file" do
   group "root"
   mode "0755"
   owner "ubuntu"
-  path "/etc/apache2/sites-enabled/kemper
+  path "/etc/apache2/sites-enabled/kemper"
   source "kemper"  
 end
