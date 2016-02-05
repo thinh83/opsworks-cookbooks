7773aae4780723ac8b0d9474b47f9f65d54f1cf4
Thinh Le <tle@kemper.com>
2016-02-05 13:56:08 -0500

    updated default.rb to include copy directive

--- files/recipes/default.rb
+++ files/recipes/default.rb
@@ -5,3 +5,11 @@ file "Create a file" do
   owner "ubuntu"
   path "/var/www/index.html"
 end
+
+cookbook_file "Copy a file" do  
+  group "root"
+  mode "0755"
+  owner "ubuntu"
+  path "/etc/apache2/sites-enabled/kemper"
+  source "kemper"  
+end
